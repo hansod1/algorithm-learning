b025c63fc1eb12e74ea9804eb80df0785d4ebfa7
Derek Hanson <hansod1@gmail.com>
2016-10-08 14:42:14 -0700

    run script and main class

---
+++ bin/DecisionTree/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+source ../run.conf
+
+java -cp $projectJar org.hansod1.algorithms.DecisionTree.Run
---
+++ src/main/scala/org/hansod1/algorithms/DecisionTree/Run.scala
@@ -0,0 +1,12 @@
+package org.hansod1.algorithms.DecisionTree
+
+/**
+  * Created by dhanson on 10/8/16.
+  */
+object Run {
+
+  def main(args: Array[String]) : Unit = {
+    println("It works!")
+  }
+
+}
